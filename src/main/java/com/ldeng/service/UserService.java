19a9e4734d17ec9b298b6ce12772ee541390a015
PaulNosenko <pavelgadzenko@gmail.com>
2018-03-11 19:17:09 +0100

    Add files via upload

---
+++ src/main/java/com/ldeng/service/UserService.java
@@ -0,0 +1,57 @@
+package com.ldeng.service;
+
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ldeng.domain.User;
+import com.ldeng.domain.UserRole;
+import com.ldeng.repository.RoleRepository;
+import com.ldeng.repository.UserRepository;
+
+@Service
+@Transactional(readOnly = true)
+public class UserService {
+	@Autowired
+	private RoleRepository roleRepository;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+
+	/** The application logger */
+	private static final Logger LOG = LoggerFactory.getLogger(UserService.class);
+
+	@Transactional
+	public User createUser(User user, Set<UserRole> userRoles) {
+
+		User localUser = userRepository.findByUsername(user.getUsername());
+
+		if (localUser != null) {
+			LOG.info("User with username {} already exist. Nothing will be done. ", user.getUsername());
+		} else {
+
+			String encryptedPassword = passwordEncoder.encode(user.getPassword());
+			user.setPassword(encryptedPassword);
+
+			for (UserRole ur : userRoles) {
+				roleRepository.save(ur.getRole());
+			}
+
+			user.getUserRoles().addAll(userRoles);
+
+			localUser = userRepository.save(user);
+
+		}
+
+		return localUser;
+
+	}
+}
